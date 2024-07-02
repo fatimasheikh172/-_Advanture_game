d8f6978078ea42257540d6c5ddc89503b9143a7e
githubusername <githubemail>
2024-07-03 00:26:19 +0500

    This is cli based Advanture game

---
+++ index.ts
@@ -0,0 +1,198 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+class Player{
+    name: string
+    fuel:number = 100;
+    constructor(name:string){
+        this.name = name
+    }
+     fuelDecrease(){
+        let fuel = this.fuel - 25
+        this.fuel = fuel
+     }
+    fuelinDecrease(){
+        this.fuel = 100
+    }
+}
+
+class opponent{
+    name: string
+    fuel:number = 100;
+    constructor(name:string){
+        this.name = name
+    }
+    fuelDecrease(){
+        let fuel = this.fuel - 25
+        this.fuel = fuel
+     }
+}
+
+let player = await inquirer.prompt(
+    [
+       {
+           name : "name",
+           type : "input",
+           message: "Please enter Your name."
+       }
+    ]
+);
+
+let Opponent = await inquirer.prompt(
+    [
+        {
+            name : "Select",
+            type : "list",
+            message: "Select your Opponent",
+            choices: ["Skeleton","Alien","Zombie"]
+        }
+    ]
+);
+
+let p1 = new Player (player.name);
+let o1 = new opponent (Opponent.Select)
+
+do{
+       // Skeleton
+
+    if(Opponent.Select === "Skeleton"){
+        let ask = await inquirer.prompt(
+            [
+                {
+                    name:"opt",
+                    type: "list",
+                    message: "What would you like to do?",
+                    choices: ["Attack","Drink portion","Run for your life.."]
+                }
+            ]
+        )
+        if(ask.opt == "Attack"){
+            let num = Math.floor(Math.random() * 2)
+            if(num > 0){
+                p1.fuelDecrease()
+                console.log(`${p1.name} fuel is ${p1.fuel}`)
+                console.log(`${o1.name} fuel is ${o1.fuel}`)
+                if(p1.fuel <= 0){
+                    console.log("you loose, Better luck next time");
+                    process.exit()
+                    
+                }
+            }
+            if(num <= 0){
+                o1.fuelDecrease()
+                console.log(`${p1.name} fuel is ${p1.fuel}`)
+                console.log(`${o1.name} fuel is ${o1.fuel}`)
+                if(o1.fuel <= 0){
+                    console.log("congratulations, you win");
+                    process.exit()
+                    
+                }
+            }
+        }
+        if(ask.opt == "Drink portion"){
+            p1.fuelinDecrease()
+            console.log(`you Drink Health portion your fuel is ${p1.fuel}`)
+        }
+        if(ask.opt == "Run for your life.."){
+            console.log("Game over ,Better luck for next time")
+            process.exit()
+        }
+    }
+
+        // Alien
+
+        if(Opponent.Select === "Alien"){
+            let ask = await inquirer.prompt(
+                [
+                    {
+                        name:"opt",
+                        type: "list",
+                        message: "What would you like to do?",
+                        choices: ["Attack","Drink portion","Run for your life.."]
+                    }
+                ]
+            )
+            if(ask.opt == "Attack"){
+                let num = Math.floor(Math.random() * 2)
+                if(num > 0){
+                    p1.fuelDecrease()
+                    console.log(`${p1.name} fuel is ${p1.fuel}`)
+                    console.log(`${o1.name} fuel is ${o1.fuel}`)
+                    if(p1.fuel <= 0){
+                        console.log("you loose, Better luck next time");
+                        process.exit()
+                        
+                    }
+                }
+                if(num <= 0){
+                    o1.fuelDecrease()
+                    console.log(`${p1.name} fuel is ${p1.fuel}`)
+                    console.log(`${o1.name} fuel is ${o1.fuel}`)
+                    if(o1.fuel <= 0){
+                        console.log("congratulations, you win");
+                        process.exit()
+                        
+                    }
+                }
+            }
+            if(ask.opt == "Drink portion"){
+                p1.fuelinDecrease()
+                console.log(`you Drink Health portion your fuel is ${p1.fuel}`)
+            }
+            if(ask.opt == "Run for your life.."){
+                console.log("Game over ,Better luck for next time")
+                process.exit()
+            }
+    }
+
+        // Zombie
+
+    if(Opponent.Select === "Zombie"){
+        let ask = await inquirer.prompt(
+            [
+                {
+                    name:"opt",
+                    type: "list",
+                    message: "What would you like to do?",
+                    choices: ["Attack","Drink portion","Run for your life.."]
+                }
+            ]
+        )
+        if(ask.opt == "Attack"){
+            let num = Math.floor(Math.random() * 2)
+            if(num > 0){
+                p1.fuelDecrease()
+                console.log(`${p1.name} fuel is ${p1.fuel}`)
+                console.log(`${o1.name} fuel is ${o1.fuel}`)
+                if(p1.fuel <= 0){
+                    console.log("you loose, Better luck next time");
+                    process.exit()
+                    
+                }
+            }
+            if(num <= 0){
+                o1.fuelDecrease()
+                console.log(`${p1.name} fuel is ${p1.fuel}`)
+                console.log(`${o1.name} fuel is ${o1.fuel}`)
+                if(o1.fuel <= 0){
+                    console.log("congratulations, you win");
+                    process.exit()
+                    
+                }
+            }
+        }
+        if(ask.opt == "Drink portion"){
+            p1.fuelinDecrease()
+            console.log(`you Drink Health portion your fuel is ${p1.fuel}`)
+        }
+        if(ask.opt == "Run for your life.."){
+            console.log("Game over ,Better luck for next time")
+            process.exit()
+        }
+}
+}
+
+while(true)
+
+
